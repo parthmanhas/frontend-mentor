b191c18ae82a0483a82500d0d00d5ba0a50a443b
parthmanhas <parthmanhas@gmail.com>
2023-04-03 23:49:23 +0530

    add firebase integration, error toast

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+import { getFirestore, collection, getDocs, setDoc, doc } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js";
 let pageOneContainer;
 let pageTwoContainer;
 let skills;
@@ -27,7 +29,51 @@ window.onscroll = function () {
 //         niceToMeetYouText.style.marginTop = `0`;
 // })
 
-window.onload = function () {
+function validateField(field, fieldName, onSubmit = false) {
+    const correctNameRegex = new RegExp('^[a-zA-Z ]+$');
+    const correctEmailRegex = new RegExp('^[a-z.0-9]+@[a-z]+.[a-z]+$');
+    const error = field.parentNode.querySelector('.error-text');
+    let fieldValid = true;
+    field.classList.remove('valid-border');
+    field.classList.remove('error-border');
+    if (field.value.length === 0) {
+        field.classList.add('error-border');
+        error.textContent = 'Please fill in the field';
+        fieldValid = false;
+    }
+    else if (fieldName === 'name' && !correctNameRegex.test(field.value)) {
+        field.style.borderBottom = '2px solid $red';
+        field.classList.add('error-border');
+        fieldValid = false;
+        error.textContent = 'Please enter only alphabets';
+    } else if (fieldName === 'email' && !correctEmailRegex.test(field.value)) {
+        field.classList.add('error-border');
+        error.textContent = 'Please enter in correct format';
+        fieldValid = false;
+    } else {
+        field.classList.add('valid-border');
+        error.textContent = '';
+    }
+
+    if (onSubmit) {
+        return fieldValid;
+    }
+}
+
+window.onload = async function () {
+
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    const messages = collection(db, 'messages');
+    const user = await getDocs(messages);
+    console.log(user)
+
     pageOneContainer = document.querySelector('.page-one-container');
     imgContainer = pageOneContainer.querySelector('.img-container');
     niceToMeetYouText = pageOneContainer.querySelector('h1:first-of-type');
@@ -65,7 +111,7 @@ window.onload = function () {
     const emailInput = form.querySelector('.email-container > input');
     const messageInput = form.querySelector('.message-container > textarea');
     [nameInput, emailInput, messageInput].forEach(input => input.addEventListener('invalid', (e) => e.preventDefault()));
-    form.addEventListener('submit', (e) => {
+    form.addEventListener('submit', async (e) => {
         e.preventDefault();
 
         const nameInputValid = validateField(nameInput, 'name', true);
@@ -73,56 +119,50 @@ window.onload = function () {
         const messageInputValid = validateField(messageInput, 'message', true);
 
         const formValid = nameInputValid && emailInputValid && messageInputValid;
+        const docData = {
+            name: nameInput.value,
+            email: emailInput.value,
+            message: messageInput.value
+        }
+        let dataSentSuccess = true;
+        if (nameInputValid && emailInputValid && messageInputValid) {
+            try {
+                await setDoc(doc(db, 'messages', docData.email), docData);
+            } catch (e) {
+                dataSentSuccess = false;
+            }
+        }
 
         // display toast
-        const toast = document.querySelector('.toast-success');
-        if (formValid && (!toast.style.display || toast.style.display === 'none')) {
-            toast.style.animation = '';
-            toast.style.display = 'block';
-            setTimeout(() => {
-                toast.style.animation = 'fade 1s linear';
-                const animationEndCleanup = () => {
-                    toast.style.display = 'none';
-                    toast.style.animation = '';
-                    toast.removeEventListener('animationend', animationEndCleanup);
-                }
-                toast.addEventListener('animationend', animationEndCleanup);
-            }, 1000);
+        const toastSuccess = document.querySelector('.toast-success');
+        const toastError = document.querySelector('.toast-error');
+        
+        function displayToast(toast, dataSentSuccess) {
+            const lineDiv = document.createElement('div');
+            if (dataSentSuccess) {
+                toast.textContent = 'Message Sent Successfully!';
+                lineDiv.classList.add('line');
+            } else {
+                toast.textContent = 'Error Sending Message!';
+                lineDiv.classList.add('error-line');
+            }
+            if ((!toast.style.display || toast.style.display === 'none')) {
+                toast.style.animation = '';
+                toast.style.display = 'block';
+                toast.appendChild(lineDiv);
+                setTimeout(() => {
+                    toast.style.animation = 'fade 1s linear';
+                    const animationEndCleanup = () => {
+                        toast.style.display = 'none';
+                        toast.style.animation = '';
+                        toast.removeEventListener('animationend', animationEndCleanup);
+                    }
+                    toast.addEventListener('animationend', animationEndCleanup);
+                }, 1000);
+            }
         }
 
-        if (nameInputValid && emailInputValid && messageInputValid) {
-            console.log('hit da endpoint');
-        }
+        dataSentSuccess ? displayToast(toastSuccess, dataSentSuccess) : displayToast(toastError, dataSentSuccess);
     })
 }
 
-function validateField(field, fieldName, onSubmit = false) {
-    const correctNameRegex = new RegExp('^[a-zA-Z ]+$');
-    const correctEmailRegex = new RegExp('^[a-z.0-9]+@[a-z]+.[a-z]+$');
-    const error = field.parentNode.querySelector('.error-text');
-    let fieldValid = true;
-    field.classList.remove('valid-border');
-    field.classList.remove('error-border');
-    if (field.value.length === 0) {
-        field.classList.add('error-border');
-        error.textContent = 'Please fill in the field';
-        fieldValid = false;
-    }
-    else if (fieldName === 'name' && !correctNameRegex.test(field.value)) {
-        field.style.borderBottom = '2px solid $red';
-        field.classList.add('error-border');
-        fieldValid = false;
-        error.textContent = 'Please enter only alphabets';
-    } else if (fieldName === 'email' && !correctEmailRegex.test(field.value)) {
-        field.classList.add('error-border');
-        error.textContent = 'Please enter in correct format';
-        fieldValid = false;
-    } else {
-        field.classList.add('valid-border');
-        error.textContent = '';
-    }
-
-    if (onSubmit) {
-        return fieldValid;
-    }
-}
