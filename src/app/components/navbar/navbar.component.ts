15994ba0e2f647f02702f4b86f201fb7cadb6879
parthmanhas <parthmanhas@gmail.com>
2023-05-21 19:47:11 +0530

    fix minor issues

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, Output, EventEmitter } from "@angular/core";
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { Store } from "@ngrx/store";
-import { AppState } from "src/app/state/app.state";
+import { deleteBoard, editBoard } from "src/app/state/app.actions";
+import { AppState, Board } from "src/app/state/app.state";
 
 @Component({
     selector: 'app-navbar',
@@ -14,17 +16,31 @@ export class NavbarComponent {
 
     public addNewTaskButtonDisabled = true;
 
+    public showBoardOptionsModal = false;
+    public edit = false;
+    public deleteBoardModalVisible = false;
+    public currentBoard: Board | null = null;
+
     constructor(private store: Store<{ app: AppState }>) {
         this.store.select(state => state).subscribe(state => {
             const currentBoardName = state.app.currentBoard;
-            const currentBoard = state.app.boards.filter(b => b.name === currentBoardName)[0];
-            this.addNewTaskButtonDisabled = !((currentBoard?.columns?.length ?? 0) > 0);
+            this.currentBoard = state.app.boards.filter(b => b.name === currentBoardName)[0];
+            this.addNewTaskButtonDisabled = !((this.currentBoard?.columns?.length ?? 0) > 0);
         });
     }
 
     public buttonClick(buttonName: string): void {
-        if (buttonName === 'addNewTask') {
+        if (buttonName === 'addNewTask' && !this.addNewTaskButtonDisabled) {
             this.createNewTask.emit(true);
         }
+    }
+
+    deleteBoard() {
+        this.deleteBoardModalVisible = false;
+        this.store.dispatch(deleteBoard({ board: this.currentBoard! }));
+    }
+
+    showEditBoardModal() {
+        this.store.dispatch(editBoard({ editBoardModalVisible: true }));
     }
 }
--- src/app/components/view-task/view-task.component.ts
+++ src/app/components/view-task/view-task.component.ts
@@ -1,101 +1,119 @@
 import { Component, EventEmitter, HostListener, Input, Output } from "@angular/core";
 import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from "@angular/forms";
 import { Store } from "@ngrx/store";
-import { addNewTask } from "src/app/state/app.actions";
+import { first } from "rxjs/operators";
+import { addNewTask, deleteTask, toggleEditTask, toggleViewTask, updateSubtasksStatus, updateTask, updateTaskStatus } from "src/app/state/app.actions";
 import { Board, AppState, Subtask, Task } from "src/app/state/app.state";
-import { v4 as uuidv4 } from 'uuid';
+import { BaseModalComponent } from "../base-modal/base-modal.component";
 
 @Component({
     selector: 'app-view-task',
     templateUrl: './view-task.component.html',
     styleUrls: ['./view-task.component.scss']
 })
-export class ViewTaskComponent {
-
-    @Input()
-    public task: Task = {
-        title: "Research pricing points of various competitors and trial different business models",
-        description: "We know what we're planning to build for version one. Now we need to finalise the first pricing model we'll use. Keep iterating the subtasks until we have a coherent proposition.",
-        status: "Doing",
-        parentColumnName: "",
-        parentBoardName: "",
-        subtasks: [
-            { title: 'Research competitor pricing and business models', isCompleted: false, parentBoardName: '', parentColumnName: '', parentTaskTitle: '' },
-            { title: 'Outline a business model that works for our solution', isCompleted: false, parentBoardName: '', parentColumnName: '', parentTaskTitle: '' },
-            { title: 'Talk to potential customers about our proposed solution and ask for fair price expectancy', isCompleted: false, parentBoardName: '', parentColumnName: '', parentTaskTitle: '' }
-        ]
-    };
-
-    public currentBoard: Board | null = null;
-
-    public completeSubtasks: Subtask[];
-    public incompleteSubtasks: Subtask[];
-
-    public options: string[] = [];
-
-    public isDropdownOpen: boolean = false;
-    public selectedOption: string = '';
-    public form: FormGroup;
-
-    public submitted = false;
-
-    @Output()
-    public closeModalEvent = new EventEmitter<boolean>();
-
-    @HostListener('document:click', ['$event'])
-    onClick(event: MouseEvent) {
-        if (this.isOutsideModalClicked(event.target)) {
-            // Click occurred outside the modal
-            this.closeModalEvent.emit(true);
+export class ViewTaskComponent extends BaseModalComponent {
+    override whenClickOccuredOutsideModal(): void {
+        if (this.task?.parentColumnName !== this.selectedOption) {
+            this.store.dispatch(updateTaskStatus({ task: this.task!, status: this.selectedOption }));
         }
-    }
-
-    constructor(private store: Store<{ app: AppState }>, private formBuilder: FormBuilder) {
+        this.store.dispatch(updateSubtasksStatus({ task: { ...this.task!, subtasks: this.form.value['subtasks'] } }));
+        this.store.dispatch(toggleViewTask({ viewTaskModalVisible: false, task: null }));
         this.form = this.formBuilder.group({
-            subtasks: this.formBuilder.array<{ isCompleted: boolean, title: string }>([])
+            subtasks: this.formBuilder.array<Subtask>([]),
+            selectedOption: [] // alias: status, parentColmnName
         });
-        // const subtasksFormArray = this.form.get('subtasks') as FormArray;
-        this.task.subtasks?.forEach((subtask) => {
-            // const subtaskFormGroup = this.formBuilder.group({
-            //     title: [subtask.title],
-            //     isCompleted: [subtask.isCompleted]
-            // });
-            // subtasksFormArray.push(subtaskFormGroup);
-            this.subtasksFormArray.push(this.formBuilder.group({
-                isCompleted: [subtask.isCompleted],
-                title: [subtask.title]
-            }))
-        });
-        console.log(this.form)
-        this.form.valueChanges.subscribe(val => {
-            // this.store.dispatch(updateSubtask({ subtask }));
-        })
-        this.completeSubtasks = this.task.subtasks?.filter(st => st.isCompleted) || [];
-        this.incompleteSubtasks = this.task.subtasks?.filter(st => !st.isCompleted) || [];
     }
-
-    isOutsideModalClicked(target: EventTarget | null): boolean {
-        // Check if the target element or any of its parents have the "modal" class
-        if (target instanceof Element && target.localName === 'app-add-new-task') {
+    override checkIfOutsideModalClicked(target: EventTarget | null): boolean {
+        if (target instanceof Element && target.localName === 'app-view-task') {
             return true;
         }
+
         return false;
+    }
+    override addFormControls(): void {
+        this.form.addControl('title', new FormControl(''));
+        this.form.addControl('description', new FormControl(''));
+        this.form.addControl('subtasks', new FormArray([]));
+        this.form.addControl('selectedOption', new FormControl('')); // alias: status, parentColmnName));
+
+    }
+    override submitWhenFormValid(): void {
+        throw new Error("Method not implemented.");
+    }
+
+    public task: Task | undefined | null;
+
+    public completeSubtasks: Subtask[] = [];
+    public incompleteSubtasks: Subtask[] = [];
+
+    public showOptionsModal = false;
+
+    public options: string[] | undefined = [];
+
+    public deleteTaskModalVisible = false;
+
+    constructor(private store: Store<{ app: AppState }>, private formBuilder: FormBuilder) {
+        super();
+        this.store.select(state => state).subscribe(state => {
+            this.task = state.app.currentTask;
+            this.options = state.app.boards.filter(b => b.name === this.task?.parentBoardName)[0]?.columns?.map(c => c.name);
+            this.form.get('selectedOption')?.setValue(this.task?.parentColumnName);
+            this.form.get('title')?.setValue(this.task?.title);
+            this.form.get('description')?.setValue(this.task?.description);
+            this.subtasksFormArray.clear();
+            this.task?.subtasks?.forEach((subtask) => {
+                this.subtasksFormArray.push(this.formBuilder.group({
+                    isCompleted: [subtask.isCompleted],
+                    title: [subtask.title]
+                }))
+            });
+
+            this.completeSubtasks = this.task?.subtasks?.filter(st => st.isCompleted) || [];
+            this.incompleteSubtasks = this.task?.subtasks?.filter(st => !st.isCompleted) || [];
+        })
     }
 
     get subtasksFormArray(): FormArray<FormGroup> {
         return this.form.get('subtasks') as FormArray<FormGroup>;
     }
 
-    toggleDropdown() {
-        this.isDropdownOpen = !this.isDropdownOpen;
+    getTitleFormControl(index: number): FormControl {
+        return this.subtasksFormArray?.at(index)?.get('title') as FormControl;
     }
 
-    get filteredOptions(): string[] {
-        return this.options.filter(option => option !== this.selectedOption);
+    get filteredOptions(): string[] | undefined {
+        return this.options?.filter(option => option !== this.selectedOption);
+    }
+
+    get selectedOption() {
+        return this.form.get('selectedOption')?.value;
     }
 
     selectOption(option: string) {
-        this.selectedOption = option;
         this.isDropdownOpen = false;
+        this.form.get('selectedOption')?.setValue(option);
+    }
+
+    deleteTask() {
+        this.store.dispatch(deleteTask({ task: this.task! }));
+        this.deleteTaskModalVisible = false;
+    }
+    save() {
+        const updatedTask = { ...this.task, ...this.form.value };
+        this.store.dispatch(updateTask({ previousTask: this.task!, updatedTask }));
+    }
+
+    toggleCheckbox(index: number): void {
+        const subtaskFormGroup = this.subtasksFormArray.at(index);
+        const isCompletedControl = subtaskFormGroup.get('isCompleted');
+
+        if (isCompletedControl) {
+            isCompletedControl.setValue(!isCompletedControl.value);
+        }
+    }
+
+    showEditTaskModal() {
+        this.showOptionsModal = false;
+        this.store.dispatch(toggleEditTask({ editTaskModalVisible: true, task: undefined }));
     }
 }
