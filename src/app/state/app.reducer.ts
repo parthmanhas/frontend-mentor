e34955bd346f3122588519dd2e91a137d5335f5c
parthmanhas <parthmanhas@gmail.com>
2023-06-10 22:41:19 +0530

    reduce code complexity

--- src/app/state/app.reducer.ts
+++ src/app/state/app.reducer.ts
@@ -33,49 +33,38 @@ export const appReducer = createReducer(
         currentTask: null,
         boards: state.boards.filter(b => b.id !== board.id)
     })),
-    on(Actions.addColumn, (state, { columns }) => ({
-        ...state,
-        boards: state.boards.map(b => {
-            if (b.id === columns[0].parentBoardId) {
-                return {
-                    ...b,
-                    columns: [...b.columns || [], ...columns]
-                }
-            }
-            return b;
-        })
-    })),
-    on(Actions.deleteColumn, (state, { column }) => ({
-        ...state,
-        boards: state.boards.map(b => {
-            if (b.id === column.parentBoardId) {
-                return {
-                    ...b,
-                    columns: b.columns?.filter(c => c.id !== column.id)
-                }
-            }
-            return b;
-        })
-    })),
-    on(Actions.addNewTask, (state, { task, addNewTaskModalVisible }) => ({
-        ...state,
-        addNewTaskModalVisible: addNewTaskModalVisible,
-        boards: state.boards.map(b => {
-            if (b.id === task.parentBoardId && b.columns) {
-                const updatedColumns = b.columns.map(c => {
-                    // task.status is the column name to which it belongs
-                    if (c.id === task.parentColumnId) {
-                        const updatedTasks = c.tasks ? [...c.tasks, task] : [task]
-                        return { ...c, tasks: updatedTasks } as Column;
-                    }
-                    return c;
-                })
-                return { ...b, columns: updatedColumns } as Board;
-            }
-            return b;
-        })
-    })),
+    on(Actions.addColumn, (state, { columns }) => {
+        const { parentBoardId } = columns[0];
+        const boardToUpdate = state.boards.find(b => b.id === parentBoardId);
+        if (!boardToUpdate) return state;
 
+        const updatedColumns = [...(boardToUpdate.columns || []), ...columns];
+        const updatedBoards = state.boards.map(b => (b.id === parentBoardId ? { ...b, columns: updatedColumns } satisfies Board : b));
+        return { ...state, boards: updatedBoards } satisfies AppState;
+    }),
+    on(Actions.deleteColumn, (state, { column }) => {
+        const { parentBoardId } = column;
+        const boardToUpdate = state.boards.find(b => b.id === parentBoardId);
+        if (!boardToUpdate) return state;
+
+        const updatedColumns = boardToUpdate.columns?.filter(c => c.id !== column.id);
+        const updatedBoards = state.boards.map(b => (b.id === parentBoardId ? { ...b, columns: updatedColumns } satisfies Board : b));
+        return { ...state, boards: updatedBoards } satisfies AppState;
+    }),
+    on(Actions.addNewTask, (state, { task, addNewTaskModalVisible }) => {
+        const { parentBoardId } = task;
+        const { parentColumnId } = task;
+        const boardToUpdate = state.boards.find(b => b.id === parentBoardId);
+        if (!boardToUpdate) return state;
+
+        const columnToUpdate = boardToUpdate.columns?.find(c => c.id === parentColumnId);
+        if (!columnToUpdate) return state;
+
+        const updatedTasks = [...(columnToUpdate.tasks || []), task];
+        const updatedColumns = boardToUpdate.columns?.map(c => c.id === parentColumnId ? ({ ...c, tasks: updatedTasks } satisfies Column) : c);
+        const updatedBoards = state.boards.map(b => b.id === parentBoardId ? ({ ...b, columns: updatedColumns } satisfies Board) : b);
+        return { ...state, boards: updatedBoards, addNewTaskModalVisible };
+    }),
     on(Actions.editBoard, (state, { editBoardModalVisible }) => ({
         ...state,
         editBoardModalVisible
@@ -108,157 +97,126 @@ export const appReducer = createReducer(
         ...state,
         addNewColumnModalVisible
     })),
-    on(Actions.updateColumnName, (state, { currentColumn, latestColumnName }) => ({
-        ...state,
-        boards: state.boards.map(b => {
-            if (b.id === currentColumn.parentBoardId) {
-                const updatedColumns = b.columns?.map(c => {
-                    if (c.id === currentColumn.id) {
-                        return { ...c, name: latestColumnName };
-                    }
-                    return c;
-                })
-                return { ...b, columns: updatedColumns }
-            }
-            return b;
-        }),
-        currentColumn: currentColumn.id
-    })),
-    on(Actions.addNewSubTask, (state, { subtask }) => ({
-        ...state,
-        boards: state.boards.map(b => {
-            if (b.id === subtask.parentBoardId && b.columns) {
-                const updatedColumns = b.columns.map(c => {
-                    if (c.id === subtask.parentColumnId && c.tasks) {
-                        const updatedTasks = c.tasks.map(t => {
-                            if (t.id === subtask.parentTaskId) {
-                                const updatedSubtasks = t.subtasks ? [...t.subtasks, subtask] : [subtask]
-                                return { ...t, subtasks: updatedSubtasks };
-                            }
-                            return t;
-                        })
-                        return { ...c, tasks: updatedTasks };
-                    }
-                    return c;
-                })
-                return { ...b, columns: updatedColumns };
-            }
-            return b;
-        })
-    })),
     on(Actions.toggleViewTask, (state, { viewTaskModalVisible, task }) => ({
         ...state,
         viewTaskModalVisible,
         currentTask: task
     })),
-    on(Actions.updateTaskParentColumn, (state, { task, newColumnId }) => ({
-        ...state,
-        currentTask: { ...state.currentTask!, parentColumnId: newColumnId },
-        boards: state.boards.map(b => {
-            if (b.id === task.parentBoardId) {
-                const updatedColumns = b.columns?.map(c => {
-                    if (c.id === task.parentColumnId) {
-                        const updatedTasks = c.tasks?.filter(t => t.id !== task.id);
-                        return { ...c, tasks: updatedTasks };
-                    } else if (c.id === newColumnId) {
-                        const updatedTasks = c.tasks ? [...c.tasks, { ...task, parentColumnId: newColumnId }] : [{ ...task, parentColumnId: newColumnId }];
-                        return { ...c, tasks: updatedTasks };
-                    }
-                    return c;
-                })
-                return { ...b, columns: updatedColumns }
-            }
-            return b;
-        })
-    })),
-    on(Actions.updateSubtasksStatus, (state, { task }) => ({
-        ...state,
-        currentTask: task,
-        boards: state.boards.map(b => {
-            if (b.id === task.parentBoardId) {
-                const updatedColumns = b.columns?.map(c => {
-                    if (c.id === task.parentColumnId) {
-                        const updatedTasks = c.tasks?.map(t => {
-                            if (t.id === task.id) {
-                                return { ...t, subtasks: task.subtasks };
-                            }
-                            return t;
-                        })
-                        return { ...c, tasks: updatedTasks };
-                    }
-                    return c;
-                })
-                return { ...b, columns: updatedColumns }
-            }
-            return b;
-        })
-    })),
-    on(Actions.deleteTask, (state, { task, viewTaskModalVisible }) => ({
-        ...state,
-        currentTask: null,
-        viewTaskModalVisible: viewTaskModalVisible ?? state.viewTaskModalVisible,
-        boards: state.boards.map(b => {
-            if (b.id === task?.parentBoardId) {
-                const updatedColumns = b.columns?.map(c => {
-                    if (c.id === task.parentColumnId) {
-                        const updatedTasks = c.tasks?.filter(t => t.id !== task.id);
-                        return { ...c, tasks: updatedTasks };
-                    }
-                    return c;
-                })
-                return { ...b, columns: updatedColumns }
-            }
-            return b;
-        })
-    })),
-    on(Actions.updateTask, (state, { task }) => ({
-        ...state,
-        currentTask: task,
-        boards: state.boards.map(b => {
-            if (b.id === task.parentBoardId) {
-                const updatedColumns = b.columns?.map(c => {
-                    if (c.id === task.parentColumnId) {
-                        const filteredTaskAfterRemovingPreviousTask = c.tasks?.filter(t => t.id !== task.id) || [];
-                        return { ...c, tasks: [...filteredTaskAfterRemovingPreviousTask, task] }
-                    }
-                    return c;
-                })
-                return { ...b, columns: updatedColumns }
-            }
-            return b;
-        })
-    })),
-    on(Actions.updateBoard, (state, { boardId, latestBoardName, columns }) => ({
-        ...state,
-        viewTaskModalVisible: false,
-        currentBoard: boardId,
-        boards: state.boards.map(b => {
-            if (b.id === boardId) {
-                let updatedColumns = b.columns?.map(c => {
-                    let latestData!: { id: string, name: string, parentBoardId: string };
-                    columns = columns?.filter(col => {
-                        if (col.id === c.id) {
-                            latestData = col;
-                            return false;
-                        }
-                        return true;
-                    });
-                    if (latestData) {
-                        return { ...c, name: latestData.name };
-                    }
-                    return { ...c };
-                    // return { ...c, parentBoardName: latestBoardName };
-                })
-                if (columns) updatedColumns?.push(...columns);
-                return { ...b, columns: updatedColumns, name: latestBoardName }
-            }
-            return b;
-        })
-    })),
+    on(Actions.updateColumnName, (state, { column, updatedName }) => {
+        const boardToUpdate = state.boards.find(b => b.id === column.parentBoardId);
+        const updatedColumns = boardToUpdate?.columns?.map(c => c.id === column.id ? { ...c, name: updatedName } satisfies Column : c);
+        const updatedBoards = state.boards.map(b => b.id === column.parentBoardId ? { ...b, columns: updatedColumns } satisfies Board : b);
+        return { ...state, boards: updatedBoards } satisfies AppState;
+
+    }),
+    on(Actions.addNewSubTask, (state, { subtask }) => {
+        const boardToUpdate = state.boards.find(b => b.id === subtask.parentBoardId);
+        if (!boardToUpdate) throw new Error('boardToUpdate empty!');
+
+        const columnToUpdate = boardToUpdate.columns?.find(c => c.id === subtask.parentColumnId);
+        if (!columnToUpdate) throw new Error('columnToUpdate empty!');
+
+        const taskToUpdate = columnToUpdate.tasks?.find(t => t.id === subtask.parentTaskId);
+        if (!taskToUpdate) throw new Error('taskToUpdate empty!');
+
+        const updatedTask = { ...taskToUpdate, subtasks: [...(taskToUpdate?.subtasks || []), subtask] } satisfies Task;
+        const updatedColumn = { ...columnToUpdate, tasks: columnToUpdate.tasks?.map(t => t.id === subtask.parentTaskId ? updatedTask : t) } satisfies Column;
+        const updatedBoard = { ...boardToUpdate, columns: boardToUpdate.columns?.map(c => c.id === subtask.parentColumnId ? updatedColumn : c) } satisfies Board;
+
+        return { ...state, boards: state.boards.map(b => b.id === subtask.parentBoardId ? updatedBoard : b) } satisfies AppState;
+    }),
+    on(Actions.updateTaskParentColumn, (state, { task, newColumnId }) => {
+        const boardToUpdate = state.boards.find(b => b.id === task.parentBoardId);
+        if (!boardToUpdate) throw new Error('boardToUpdate empty!');
+
+        const columnToRemoveTaskFrom = boardToUpdate.columns?.find(c => c.id === task.parentColumnId);
+        const columnToAddTaskTo = boardToUpdate.columns?.find(c => c.id === newColumnId);
+        if (!columnToRemoveTaskFrom) throw new Error('columnToRemoveTaskFrom empty!');
+        if (!columnToAddTaskTo) throw new Error('columnToAddTaskTo empty!');
+
+        const taskToUpdate = columnToRemoveTaskFrom.tasks?.find(t => t.id === task.id);
+        if (!taskToUpdate) throw new Error('taskToUpdate empty!');
+
+
+        const updatedTask = { ...taskToUpdate, parentColumnId: newColumnId } satisfies Task;
+        const columnWithTaskRemoved = { ...columnToRemoveTaskFrom, tasks: columnToRemoveTaskFrom.tasks?.filter(t => t.id !== task.id) } satisfies Column;
+        const columnWithTaskAdded = { ...columnToAddTaskTo, tasks: [...(columnToAddTaskTo.tasks || []), updatedTask] } satisfies Column;
+        const updatedColumns = boardToUpdate.columns?.map(c => c.id === newColumnId ? columnWithTaskAdded : c.id === task.parentColumnId ? columnWithTaskRemoved : c);
+        const updatedBoard = { ...boardToUpdate, columns: updatedColumns } satisfies Board;
+
+        return {
+            ...state,
+            boards: state.boards.map(b => b.id === task.parentBoardId ? updatedBoard : b),
+            currentTask: { ...state.currentTask!, parentColumnId: newColumnId },
+        } satisfies AppState;
+
+
+    }),
+    on(Actions.updateSubtasksStatus, (state, { task }) => {
+        const boardToUpdate = state.boards.find(b => b.id === task.parentBoardId);
+        if (!boardToUpdate) throw new Error('taskToUpdate empty');
+
+        const columnToUpdate = boardToUpdate?.columns?.find(c => c.id === task.parentColumnId);
+        if (!columnToUpdate) throw new Error('taskToUpdate empty');
+
+        const taskToUpdate = columnToUpdate?.tasks?.find(t => t.id === task.id);
+        if (!taskToUpdate) throw new Error('taskToUpdate empty');
+
+        const updatedTask = { ...taskToUpdate, subtasks: task.subtasks } satisfies Task;
+        const updatedColumn = { ...columnToUpdate, tasks: columnToUpdate?.tasks?.map(t => t.id === task.id ? updatedTask : t) } satisfies Column;
+        const updatedBoard = { ...boardToUpdate, columns: boardToUpdate?.columns?.map(c => c.id === task.parentColumnId ? updatedColumn : c) } satisfies Board;
+
+        return { ...state, boards: state.boards.map(b => b.id === task.parentBoardId ? updatedBoard : b), currentTask: task } satisfies AppState;
+    }),
+    on(Actions.deleteTask, (state, { task, viewTaskModalVisible }) => {
+        const boardToUpdate = state.boards.find(b => b.id === task.parentBoardId);
+        if (!boardToUpdate) throw new Error('boardToUpdate');
+
+        const columnToUpdate = boardToUpdate?.columns?.find(c => c.id === task.parentColumnId);
+        if (!columnToUpdate) throw new Error('columnToUpdate');
+
+        const updatedColumn = { ...columnToUpdate, tasks: columnToUpdate?.tasks?.filter(t => t.id !== task.id) } satisfies Column;
+        const updatedBoard = { ...boardToUpdate, columns: boardToUpdate?.columns?.map(c => c.id === task.parentColumnId ? updatedColumn : c) } satisfies Board;
+        const updatedBoards = state.boards.map(b => b.id === task.parentBoardId ? updatedBoard : b);
+
+        return {
+            ...state,
+            boards: updatedBoards,
+            currentTask: null,
+            viewTaskModalVisible: viewTaskModalVisible ?? state.viewTaskModalVisible,
+        } satisfies AppState
+    }),
+    on(Actions.updateTask, (state, { task }) => {
+        const boardToUpdate = state.boards.find(b => b.id === task.parentBoardId);
+        const columnToUpdate = boardToUpdate?.columns?.find(c => c.id === task.parentColumnId);
+        const updatedColumn = { ...columnToUpdate, tasks: columnToUpdate?.tasks?.map(t => t.id === task.id ? { ...t, ...task } satisfies Task : t) } as Column;
+        const updatedBoard = { ...boardToUpdate, columns: boardToUpdate?.columns?.map(c => c.id === task.parentColumnId ? updatedColumn satisfies Column : c) } as Board;
+        const updatedBoards = state.boards.map(b => b.id === task.parentBoardId ? updatedBoard : b);
+        return { ...state, boards: updatedBoards, currentTask: task, } satisfies AppState;
+    }),
+    on(Actions.updateBoard, (state, { boardId, latestBoardName, columns }) => {
+        const boardToUpdate = state.boards.find(b => b.id === boardId);
+        let updatedColumns = boardToUpdate?.columns?.map(c => ({ ...c, ...columns?.find(col => col.id === c.id) } satisfies Column));
+        // add new columns
+        const newColumns = columns?.filter(col => updatedColumns?.filter(c => c.id !== col.id).length || 0 > 0)
+        updatedColumns = [...(updatedColumns || []), ...(newColumns || [])];
+
+        const updatedBoards = state.boards.map(b => b.id === boardId ? { ...b, columns: updatedColumns, name: latestBoardName } satisfies Board : b)
+        return { ...state, boards: updatedBoards, viewTaskModalVisible: false, currentBoardId: boardId, } satisfies AppState;
+    }),
     on(Actions.setCurrentTask, (state, { task }) => ({
         ...state,
         currentTask: state.boards.filter(b => b.id === task.parentBoardId)[0]
             .columns?.filter(c => c.id === task.parentColumnId)[0]
             .tasks?.filter(t => t.id === task.id)[0]
+    })),
+    on(Actions.toggleMobile, (state, { isMobile }) => ({
+        ...state,
+        isMobile: isMobile ?? !state.isMobile,
+    })),
+    on(Actions.setMobileCss, (state, { mobileCss }) => ({
+        ...state,
+        mobileCss: mobileCss ?? state.mobileCss,
     }))
+
 );
