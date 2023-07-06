a23b365d6af892b692def46708b9760d1a6e9e4e
parthmanhas <parthmanhas@gmail.com>
2023-07-06 13:42:15 +0530

    complete remaining functions

--- src/app/pages/view-invoice/view-invoice.component.ts
+++ src/app/pages/view-invoice/view-invoice.component.ts
@@ -62,5 +62,91 @@ export class ViewInvoiceComponent {
         this.editForm.value.items?.forEach(item => this.total += (item.quantity ?? 0) * (item.price ?? 0));
     }
 
+    initItemForm(item: InvoiceItem): InvoiceItemForm {
+        return this.fb.nonNullable.group({
+            name: [item.name, Validators.required],
+            quantity: [item.quantity, [Validators.required, Validators.min(1)]],
+            price: [item.price, Validators.required],
+        });
+    }
+
+    get getStatusClass() {
+        const classes = [''];
+        switch (this.editForm.value.status) {
+            case 'paid':
+                classes.push('bg-shamrock-50 dark:bg-shamrock-400 dark:bg-opacity-5');
+                break;
+            case 'pending':
+                classes.push('bg-pizazz-50 dark:bg-pizazz-600 dark:bg-opacity-5');
+                break;
+            case 'draft':
+                classes.push('bg-oxford-blue-50 dark:bg-selago-100 dark:bg-opacity-5');
+                break;
+        }
+
+        return classes.join(' ');
+    }
+
+    // join the client address array with a <br> tag
+    getAddress() {
+        const address = this.editForm.value.client?.address;
+        return `${address?.street}<br>${address?.city}<br>${address?.postCode}<br>`;
+    }
+
+    getObjectKeys(obj: any): string[] {
+        return Object.keys(obj);
+    }
+
+    calculateTotal(quantity: number, price: number): number {
+        return quantity * price;
+    }
+
+    deleteInvoice() {
+        this.deleteFormVisible = false;
+        this.appComponent.invoices$.next(this.appComponent.invoices$.value.filter(invoice => invoice.id !== this.editForm.value.id));
+        this.router.navigate(['/view-invoices']);
+    }
+
+    toggleTheme(theme: 'light' | 'dark') {
+        this.appComponent.theme$.next(theme);
+    }
+
+    goBackToViewInvoices() {
+        this.router.navigate(['/view-invoices']);
+    }
+
+    markAsPaid() {
+        const invoice = this.appComponent.invoices$.value.find(invoice => invoice.id === this.editForm.value.id) ?? null;
+        if (invoice === null) {
+            console.error('Invoice not found');
+            return;
+        }
+        if (invoice.status === 'paid') {
+            console.info('Invoice already paid');
+            return;
+        }
+        invoice.status = 'paid';
+        this.appComponent.invoices$.next(this.appComponent.invoices$.value.map(invoice => invoice.id === this.editForm.value.id ? { ...invoice, status: 'paid' } : invoice));
+    }
+
+    markAsPending() {
+        const invoice = this.appComponent.invoices$.value.find(invoice => invoice.id === this.editForm.value.id) ?? null;
+        if (invoice === null) {
+            console.error('Invoice not found');
+            return;
+        }
+        // return if already paid
+        if (invoice.status === 'paid') {
+            console.info('Invoice already paid');
+            return;
+        }
+        if (invoice.status === 'pending') {
+            console.info('Invoice already pending');
+            return;
+        }
+        invoice.status = 'pending';
+        this.appComponent.invoices$.next(this.appComponent.invoices$.value.map(invoice => invoice.id === this.editForm.value.id ? { ...invoice, status: 'pending' } : invoice));
+
+    }
 
 }
